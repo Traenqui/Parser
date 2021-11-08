d66b45f139bb1599d0b0168fefc5604cbe29d13e
Jonas Gerber <73141623+Traenqui@users.noreply.github.com>
2021-11-08 13:39:56 +0100

    updaated list_test

--- list_test.py
+++ list_test.py
@@ -22,45 +22,9 @@ connection = sql_connect(localhost, root, password)
         - check if database exists, if not create
         - check if table exists, if not create, if yes delete content and insert new
 """
+
 all_paths = [
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_4LifeCZ_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_4LifeSK_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Annuities_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Annuities_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Annuities_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_AP_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_AP_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_AP_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_CriticalIllness_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_CriticalIllness_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_CriticalIllness_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Fibas_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Fibas_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Fibas_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_FPTDE_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_FPTDE_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_FPTDE_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_FPTIT_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_FPTIT_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_FPTIT_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_ImpairedLife_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_ImpairedLife_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_ImpairedLife_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Jool_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Jool_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Jool_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Pulse_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Pulse_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Pulse_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_SinglePolicies_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_SinglePolicies_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_SinglePolicies_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Term_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Term_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_Term_21Q2.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_UnitLinked_20Q4.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_UnitLinked_21Q1.csv',
-    '/Users/traenqui/Projects/Valucor/Parser/test_csvfile/tblOut_UnitLinked_21Q2.csv'
+
 ]
 
 # Path and variables
